6396d313d396cc5810077c7b02a584bb81931ba8
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2023-01-13 07:21:00 -0800

    all device-side gen

--- benches/cub/device/memcpy/basic.cu
+++ benches/cub/device/memcpy/basic.cu
@@ -4,16 +4,38 @@
 #include <thrust/detail/raw_pointer_cast.h>
 #include <thrust/device_vector.h>
 #include <thrust/execution_policy.h>
+#include <thrust/gather.h>
 #include <thrust/host_vector.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/permutation_iterator.h>
+#include <thrust/random.h>
+#include <thrust/scan.h>
+#include <thrust/scatter.h>
 #include <thrust/sequence.h>
+#include <thrust/shuffle.h>
 
 #include <cub/device/device_memcpy.cuh>
 #include <cub/iterator/transform_input_iterator.cuh>
+
+#include <tbm/range_generator.cuh>
 
 #include <cstdint>
 #include <limits>
 #include <random>
 #include <stdexcept>
+
+template <typename ItT, typename T>
+void __global__ prnt(ItT data, T num_items)
+{
+  if (threadIdx.x == 0)
+  {
+    for (int i = 0; i < num_items; i++)
+    {
+      auto x = data[i];
+      printf("%d: %d\n", i, x);
+    }
+  }
+}
 
 /**
  * @brief Enum class with options for generating the buffer order within memory
@@ -45,71 +67,91 @@ struct offset_to_ptr_op
 };
 
 /**
- * @brief Host-side random data generation
- */
-template <typename T>
-void generate_random_data(
-  T *rand_out,
-  const std::size_t num_items,
-  const T min_rand_val          = std::numeric_limits<T>::min(),
-  const T max_rand_val          = std::numeric_limits<T>::max(),
-  const std::uint_fast32_t seed = 320981U,
-  typename std::enable_if<std::is_integral<T>::value && (sizeof(T) >= 2)>::type
-    * = nullptr)
-{
-  // Initialize random number generator
-  std::mt19937 rng(seed);
-  std::uniform_int_distribution<T> uni_dist(min_rand_val, max_rand_val);
-
-  // Generate random numbers
-  for (std::size_t i = 0; i < num_items; ++i)
-  {
-    rand_out[i] = uni_dist(rng);
-  }
-}
-
-/**
  * @brief Used for generating a shuffled but cohesive sequence of output-buffer
  * offsets for the sequence of input-buffers.
  */
-template <typename BufferOffsetT, typename ByteOffsetT, typename BufferSizeT>
-std::vector<ByteOffsetT>
-get_shuffled_buffer_offsets(const std::vector<BufferSizeT> &buffer_sizes,
-                            const std::uint_fast32_t seed = 320981U)
+template <typename BufferOffsetT,
+          typename ByteOffsetT,
+          typename BufferSizeItT,
+          typename ByteOffsetItT>
+void get_shuffled_buffer_offsets(BufferSizeItT buffer_sizes,
+                                 BufferOffsetT num_buffers,
+                                 ByteOffsetItT shuffled_buffer_offsets,
+                                 const unsigned int seed = 320981U)
 {
-  BufferOffsetT num_buffers = static_cast<BufferOffsetT>(buffer_sizes.size());
+  num_buffers = 100;
 
   // We're remapping the i-th buffer to pmt_idxs[i]
-  std::mt19937 rng(seed);
-  std::vector<BufferOffsetT> pmt_idxs(num_buffers);
-  std::iota(pmt_idxs.begin(), pmt_idxs.end(), static_cast<BufferOffsetT>(0));
-  std::shuffle(std::begin(pmt_idxs), std::end(pmt_idxs), rng);
+  thrust::device_vector<BufferOffsetT> pmt_idxs(num_buffers);
+
+  thrust::default_random_engine g{seed};
+  auto seq_it = thrust::make_counting_iterator(BufferOffsetT{0});
+  thrust::shuffle_copy(seq_it, seq_it + num_buffers, std::begin(pmt_idxs), g);
+
+  // std::cout << " -- Buffer sizes --\n";
+  // prnt<<<1, 1>>>(buffer_sizes, num_buffers);
+
+  // std::cout << " -- Permutation indexes --\n";
+  // for (int i = 0; i < num_buffers; i++)
+  //   std::cout << i << ": " << pmt_idxs[i] << "\n";
+  // exit(-1);
 
   // Compute the offsets using the new mapping
-  ByteOffsetT running_offset = {};
-  std::vector<ByteOffsetT> permuted_offsets;
-  permuted_offsets.reserve(num_buffers);
-  for (auto permuted_buffer_idx : pmt_idxs)
-  {
-    permuted_offsets.emplace_back(running_offset);
-    running_offset += buffer_sizes[permuted_buffer_idx];
-  }
+  auto permuted_sizes_it =
+    thrust::make_permutation_iterator(buffer_sizes, std::begin(pmt_idxs));
+  thrust::device_vector<ByteOffsetT> permuted_offsets(num_buffers);
+  thrust::exclusive_scan(permuted_sizes_it,
+                         permuted_sizes_it + num_buffers,
+                         std::begin(permuted_offsets),
+                         static_cast<ByteOffsetT>(0));
+
+  // for (auto permuted_buffer_idx : pmt_idxs)
+  // {
+  //   permuted_offsets.emplace_back(running_offset);
+  //   running_offset += buffer_sizes[permuted_buffer_idx];
+  // }
 
   // Generate the scatter indexes that identify where each buffer was mapped to
-  std::vector<BufferOffsetT> scatter_idxs(num_buffers);
-  for (BufferOffsetT i = 0; i < num_buffers; i++)
-  {
-    scatter_idxs[pmt_idxs[i]] = i;
-  }
+  thrust::device_vector<BufferOffsetT> gather_map(num_buffers);
+  thrust::scatter(seq_it,
+                  seq_it + num_buffers,
+                  std::begin(pmt_idxs),
+                  std::begin(gather_map));
+  // thrust::sequence(permuted_scatter_idxs, permuted_scatter_idxs +
+  // num_buffers, BufferOffsetT{0}); for (BufferOffsetT i = 0; i < num_buffers;
+  // i++)
+  // {
+  //   scatter_idxs[pmt_idxs[i]] = i;
+  // }
 
-  std::vector<ByteOffsetT> new_offsets(num_buffers);
-  for (BufferOffsetT i = 0; i < num_buffers; i++)
-  {
-    new_offsets[i] = permuted_offsets[scatter_idxs[i]];
-  }
-
-  return new_offsets;
+  thrust::device_vector<ByteOffsetT> new_offsets(num_buffers);
+  thrust::gather(std::begin(gather_map),
+                 std::end(gather_map),
+                 std::begin(permuted_offsets),
+                 shuffled_buffer_offsets);
+  //  std::begin(new_offsets));
+  // for (BufferOffsetT i = 0; i < num_buffers; i++)
+  // {
+  //   new_offsets[i] = permuted_offsets[scatter_idxs[i]];
+  // }
 }
+
+struct offset_buffers_and_resize_op
+{
+  std::size_t min_buffer_size{};
+  std::size_t max_buffer_size{};
+  std::size_t atomic_type_size{};
+
+  template <typename T>
+  __device__ __forceinline__ T operator()(const T &val)
+  {
+    auto rnd_buffer_size = std::abs(val);
+    rnd_buffer_size = rnd_buffer_size % (max_buffer_size - min_buffer_size);
+    rnd_buffer_size = min_buffer_size + rnd_buffer_size;
+    return CUB_ROUND_DOWN_NEAREST(rnd_buffer_size,
+                                  static_cast<T>(atomic_type_size));
+  }
+};
 
 template <typename AtomicT, buffer_order buffer_order>
 static void basic(nvbench::state &state,
@@ -149,54 +191,77 @@ static void basic(nvbench::state &state,
     static_cast<std::size_t>(target_copy_size / average_buffer_size);
 
   // Buffer segment data (their offsets and sizes)
-  std::vector<BufferSizeT> h_buffer_sizes(num_buffers);
-  std::vector<ByteOffsetT> h_buffer_src_offsets(num_buffers);
-  std::vector<ByteOffsetT> h_buffer_dst_offsets(num_buffers);
+  thrust::device_vector<ByteOffsetT> h_buffer_src_offsets(num_buffers);
+  thrust::device_vector<ByteOffsetT> h_buffer_dst_offsets(num_buffers);
 
   // Generate the buffer sizes
-  generate_random_data(h_buffer_sizes.data(),
-                       h_buffer_sizes.size(),
-                       static_cast<BufferSizeT>(min_buffer_size),
-                       static_cast<BufferSizeT>(max_buffer_size));
+  auto rnd_buffer_sizes =
+    tbm::make_range_generator<BufferSizeT,
+                              tbm::iterator_style::pointer,
+                              tbm::data_pattern::random>(num_buffers);
+
+  // Offset random buffer sizes by the minimum buffer size and make sure buffer
+  // sizes are a multiple of the most granular unit (one AtomicT) being copied
+  // (round down)
+  // prnt<<<1, 1>>>(rnd_buffer_sizes.cbegin(), 100);
+  thrust::device_vector<BufferSizeT> buffer_sizes(num_buffers);
+  thrust::transform(std::cbegin(rnd_buffer_sizes),
+                    std::cend(rnd_buffer_sizes),
+                    std::begin(buffer_sizes),
+                    offset_buffers_and_resize_op{min_buffer_size,
+                                                 max_buffer_size,
+                                                 sizeof(AtomicT)});
+  // prnt<<<1, 1>>>(thrust::raw_pointer_cast(buffer_sizes.data()), 100);
 
   // Make sure buffer sizes are a multiple of the most granular unit (one
   // AtomicT) being copied (round down)
-  for (BufferOffsetT i = 0; i < num_buffers; i++)
+  //   for (BufferOffsetT i = 0; i < num_buffers; i++)
+  // {
+  //   h_buffer_sizes[i] = (h_buffer_sizes[i] / sizeof(AtomicT)) *
+  //   sizeof(AtomicT);
+  // }
+
+  // For consecutive input buffer order generate the buffer offsets
+  if (input_gen == buffer_order::CONSECUTIVE)
   {
-    h_buffer_sizes[i] = (h_buffer_sizes[i] / sizeof(AtomicT)) * sizeof(AtomicT);
+    thrust::exclusive_scan(std::begin(buffer_sizes),
+                           std::end(buffer_sizes),
+                           std::begin(h_buffer_src_offsets),
+                           0);
+  }
+  // For consecutive output buffer order generate the buffer offsets
+  if (output_gen == buffer_order::CONSECUTIVE)
+  {
+    thrust::exclusive_scan(std::begin(buffer_sizes),
+                           std::end(buffer_sizes),
+                           std::begin(h_buffer_dst_offsets),
+                           0);
   }
 
   // Compute the total bytes to be copied
-  ByteOffsetT num_total_bytes = 0;
-  for (BufferOffsetT i = 0; i < num_buffers; i++)
-  {
-    if (input_gen == buffer_order::CONSECUTIVE)
-    {
-      h_buffer_src_offsets[i] = num_total_bytes;
-    }
-    if (output_gen == buffer_order::CONSECUTIVE)
-    {
-      h_buffer_dst_offsets[i] = num_total_bytes;
-    }
-    num_total_bytes += h_buffer_sizes[i];
-  }
+  ByteOffsetT num_total_bytes = thrust::reduce(std::begin(buffer_sizes),
+                                               std::end(buffer_sizes));
 
   // Shuffle input buffer source-offsets
-  std::uint_fast32_t shuffle_seed = 320981U;
+  unsigned int shuffle_seed = 320981U;
   if (input_gen == buffer_order::RANDOM)
   {
-    h_buffer_src_offsets =
-      get_shuffled_buffer_offsets<BufferOffsetT, ByteOffsetT>(h_buffer_sizes,
-                                                              shuffle_seed);
+    get_shuffled_buffer_offsets<BufferOffsetT, ByteOffsetT>(
+      buffer_sizes.cbegin(),
+      static_cast<BufferOffsetT>(buffer_sizes.size()),
+      h_buffer_src_offsets.data(),
+      shuffle_seed);
     shuffle_seed += 42;
   }
 
   // Shuffle input buffer source-offsets
   if (output_gen == buffer_order::RANDOM)
   {
-    h_buffer_dst_offsets =
-      get_shuffled_buffer_offsets<BufferOffsetT, ByteOffsetT>(h_buffer_sizes,
-                                                              shuffle_seed);
+    get_shuffled_buffer_offsets<BufferOffsetT, ByteOffsetT>(
+      buffer_sizes.cbegin(),
+      static_cast<BufferOffsetT>(buffer_sizes.size()),
+      h_buffer_dst_offsets.data(),
+      shuffle_seed);
   }
 
   // Get temporary storage requirements
@@ -259,7 +324,7 @@ static void basic(nvbench::state &state,
     h_buffer_src_offsets;
   thrust::device_vector<ByteOffsetT> d_buffer_dst_offsets =
     h_buffer_dst_offsets;
-  thrust::device_vector<BufferSizeT> d_buffer_sizes = h_buffer_sizes;
+  thrust::device_vector<BufferSizeT> d_buffer_sizes = buffer_sizes;
 
   // Prepare d_buffer_srcs
   offset_to_ptr_op<SrcPtrT> src_transform_op{static_cast<SrcPtrT>(d_in)};
